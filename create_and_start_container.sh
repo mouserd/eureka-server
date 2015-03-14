c8b9fede66d94ade660a4ac6ac231dcc7ac8ee45
David Mouser <David.Mouser@afl.com.au>
2015-03-14 22:21:23 +1100

    Stop docker container before trying to remove it.

--- create_and_start_container.sh
+++ create_and_start_container.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+docker stop eureka-server
 docker rm eureka-server
+
 docker run \
-  -d \
   -p 8761:8761 \
   --name eureka-server \
   -e PORT=8761 \
-  -e CONFIG_URI=http://192.168.59.103:8888 eureka-server
+  -e CONFIG_URI=http://192.168.59.103:8888 \
+  -d eureka-server
